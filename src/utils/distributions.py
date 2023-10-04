779203d275fd48e37c0664aeda047eb7a0f54593
enikobankuti <eniko.bankuti@gmail.com>
2023-10-04 20:18:40 +0200

    logistic ready

--- src/utils/distributions.py
+++ src/utils/distributions.py
@@ -203,6 +203,7 @@ amely értékeket adattagokba ment le.
     bemenet: self, rand, loc, scale
 """
 
+
 class NormalDistribution:
     def __init__(self, rand, loc, scale):
         self.rand = rand
@@ -446,4 +447,169 @@ class CauchyDistribution:
     bemenet: None
     kimeneti típus: List
     link: https://en.wikipedia.org/wiki/Cauchy_distribution
+"""
+
+""" Harmadik ZH """
+
+
+class LogisticDistribution:
+    def __init__(self, rand, location, scale):
+        self.rand = rand
+        self.location = location
+        self.scale = scale
+
+    def pdf(self, x):
+        exp_term = math.exp(-(x - self.location) / self.scale)
+        pdf = exp_term / (self.scale * (1 + exp_term) ** 2)
+        return pdf
+
+    def cdf(self, x):
+        return 1 / (1 + math.exp(-(x - self.location) / self.scale))
+
+    def ppf(self, p):
+        ppf = self.location + self.scale * math.log(p / (1 - p))
+        return ppf
+
+    def gen_rand(self):
+        return self.ppf(self.rand.random())
+
+    def mean(self):
+        return self.location
+
+    def variance(self):
+        return (self.scale ** 2 * math.pi ** 2) / 3
+
+    def skewness(self):
+        return 0
+
+    def ex_kurtosis(self):
+        return 6 / 5
+
+    def mvsk(self):
+        l = [self.mean(), self.variance(), self.skewness(), self.ex_kurtosis()]
+        return l
+
+
+"""
+1., Hozzan létre egy új osztályt aminek a neve LogisticDistribution. Definiáljon benne
+a __init__ nevű függvényt, amely bemenetként kap egy véletlenszám generátort,
+és az eloszlás várható értékét (location) és szórás (scale) paramétereit,
+amely értékeket adattagokba ment le.
+
+    Osztály név: LogisticDistribution
+    függvény név: __init__
+    bemenet: self, rand, loc, scale
+    link: https://en.wikipedia.org/wiki/Logistic_distribution
+
+2., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely megvalósítja az eloszlás sűrűség függvényét.
+    függvény név: pdf
+    bemenet: x
+    kimeneti típus: float
+
+3., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely megvalósítja az eloszlás kumulatív eloszlás függvényét.
+    függvény név: cdf
+    bemenet: x
+    kimeneti típus: float
+
+4., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely implementálja az eloszlás inverz kumulatív eloszlás függvényét
+    függvény név: ppf
+    bemenet: p
+    kimeneti típus: float
+
+5., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely az osztály létrehozásánál megadott paraméterek mellett, logisztikus eloszlású véletlen számokat generál minden meghívásnál
+    függvény név: gen_rand
+    bemenet: None
+    kimeneti típus: float
+
+6., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény várható értékét. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: mean
+    bemenet: None
+    kimeneti típus: float
+
+7., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény varianciáját. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: variance
+    bemenet: None
+    kimeneti típus: float
+
+8., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény ferdeségét. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: skewness
+    bemenet: None
+    kimeneti típus: float
+
+9., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény többlet csúcsosságát. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: ex_kurtosis
+    bemenet: None
+    kimeneti típus: float
+
+10., Egészítse ki a LogisticDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény első momentumát, a 2. és 3. cetrális momentumát, és a többlet csúcsosságot.. Ha az eloszlásnak nincsenek ilyen értékei, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: mvsk
+    bemenet: None
+    kimeneti típus: List
+"""
+import scipy.special as sc
+
+
+class ChiSquaredDistribution:
+    def __init__(self, rand, dof):
+        self.rand = rand
+        self.dof = dof
+
+    def pdf(self, x):
+        return (1 / (2 ** (x / 2 - 1) * sc.gamma(x / 2))) * math.exp(-x / 2)
+
+    def cdf(self, x):
+        return sc.gammainc(x/2, x/2)
+
+"""
+11., Hozzan létre egy új osztályt aminek a neve ChiSquaredDistribution. Definiáljon benne a __init__ nevű függvényt, amelynek bemenetként kap egy véletlenszám generátort, és egy szabadságfok (dof) paramétert, amely értékeket adattagokba ment le.
+    Osztály név: ChiSquaredDistribution
+    függvény név: __init__
+    bemenet: self, rand, dof
+    link: https://en.wikipedia.org/wiki/Chi-squared_distribution
+    link: https://docs.scipy.org/doc/scipy/tutorial/stats/continuous_chi2.html
+
+12., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely megvalósítja az eloszlás sűrűség függvényét.
+    függvény név: pdf
+    bemenet: x
+    kimeneti típus: float
+
+13., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely megvalósítja az eloszlás kumulatív eloszlás függvényét.
+    függvény név: cdf
+    bemenet: x
+    kimeneti típus: float
+
+14., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely implementálja az eloszlás inverz kumulatív eloszlás függvényét
+    függvény név: ppf
+    bemenet: p
+    kimeneti típus: float
+
+15., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely az osztály létrehozásánál megadott paraméterek mellett, logisztikus eloszlású véletlen számokat generál minden meghívásnál
+    függvény név: gen_rand
+    bemenet: None
+    kimeneti típus: float
+
+16., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény várható értékét. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: mean
+    bemenet: None
+    kimeneti típus: float
+
+17., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény varianciáját. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: variance
+    bemenet: None
+    kimeneti típus: float
+
+18., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény ferdeségét. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: skewness
+    bemenet: None
+    kimeneti típus: float
+
+19., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény többlet csúcsosságát. Ha az eloszlásnak nincsen ilyen értéke, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: ex_kurtosis
+    bemenet: None
+    kimeneti típus: float
+
+20., Egészítse ki a ChiSquaredDistribution osztályt egy új függvénnyel, amely visszaadja az eloszlás függvény első momentumát, a 2. és 3. cetrális momentumát, és a többlet csúcsosságot.. Ha az eloszlásnak nincsenek ilyen értékei, akkor return helyett hívja meg a raise Exception("Moment undefined") parancsot.
+    függvény név: mvsk
+    bemenet: None
+    kimeneti típus: List
 """
