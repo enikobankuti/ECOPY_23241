82383e23b1e0d8d4e3eadd044341bb3c7b705a5a
enikobankuti <eniko.bankuti@gmail.com>
2023-11-22 20:40:46 +0100

    test9

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -2,6 +2,8 @@ import numpy as np
 import statsmodels.api as sm
 import pandas as pd
 import scipy.stats as stats
+from scipy.stats import t, norm
+from scipy.optimize import minimize
 
 
 class LinearRegressionSM:
@@ -153,3 +155,67 @@ class LinearRegressionGLS:
         crs = 1 - SSR / SST
         ars = 1 - (1 - crs) * (self.n - 1) / self.dof
         return f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
+
+
+class LinearRegressionML:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+
+    def neg_loglikelihood(self, params):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        betas = params[:-1]
+        sig = params[-1]
+        pred = np.dot(X, betas)
+        likeli = np.sum(norm.logpdf(y, pred, sig))
+        return -likeli
+
+    def calculate_res(self, X, y, coefficients):
+        return y - np.dot(X, coefficients)
+
+    def calculate_p_values(self, coefficients, X, y):
+        n, k = X.shape
+        res = self.calculate_res(X, y, coefficients)
+        sigma_squared = np.sum(res ** 2) / (n - k)
+        var_beta = np.linalg.inv(X.T @ X) * sigma_squared
+        t_statistic = coefficients / np.sqrt(np.diag(var_beta))
+        p_values = 2 * (1 - t.cdf(np.abs(t_statistic), df=n - k))
+        return p_values
+
+    def neg_loglikelihood(self, params):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        b0, b1, b2, b3, sigma_sq = params
+        betas = np.array([b0, b1, b2, b3])
+        pred = X @ betas
+        likeli = np.sum(stats.norm.logpdf(y, pred, sigma_sq))
+
+        return -likeli
+
+    def fit(self):
+        opt = minimize(self.neg_loglikelihood, np.array([.1, .1, .1, .1, .1]), method='L-BFGS-B')
+        b0, b1, b2, b3, sigma_sq = opt.x
+        self.coefficients = np.array([b0, b1, b2, b3])
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        n, k = X.shape
+        self.res = y - X @ self.coefficients
+        sigma_sq = np.sum(self.res ** 2) / (n - k)
+        var_b = np.linalg.inv(X.T @ X) * sigma_sq
+        t_stat = self.coefficients / np.sqrt(np.diag(var_b))
+        self.p_values = 2 * (1 - stats.t.cdf(np.abs(t_stat), df=n-k))
+        rss = np.sum(self.res ** 2)
+        tss = np.sum((y - np.mean(y)) ** 2)
+        self.crs = 1 - rss / tss
+        self.ars = 1 - (rss / (n - k)) / (tss / (n - 1))
+
+    def get_params(self):
+        return pd.Series(self.coefficients, name='Beta coefficients')
+
+    def get_pvalues(self):
+        index = ['Intercept'] + list(self.right_hand_side.columns)
+        return pd.Series(self.p_values, index=index, name='P-values for the corresponding coefficients')
+
+    def get_model_goodness_values(self):
+        return f'Centered R-squared: {self.crs:.3f}, Adjusted R-squared: {self.ars:.3f}'
